b763b2d704ab0c87fc8b3afeb2fc282f8783bc1a
devhoss <velasco2288@gmail.com>
2023-09-08 16:56:14 -0500

    prueba para el find all

--- src/test/java/com/devhoss/app/IntegracionJpaTest.java
+++ src/test/java/com/devhoss/app/IntegracionJpaTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,5 +31,19 @@ public class IntegracionJpaTest {
         assertTrue(cuenta.isPresent());
         assertEquals("Hossmell", cuenta.orElseThrow().getPersona());
         assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = icuentaRepository.findByPersona("Rod");
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = icuentaRepository.findAll();
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
     }
 }
