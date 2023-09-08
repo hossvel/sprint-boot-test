9ed5eb57bb44e13ebd558f7c67d9864c3e25ab0f
devhoss <velasco2288@gmail.com>
2023-09-08 16:24:21 -0500

    creando um metodo personalziado en repository

--- src/main/java/com/devhoss/app/repositories/ICuentaRepository.java
+++ src/main/java/com/devhoss/app/repositories/ICuentaRepository.java
@@ -10,8 +10,9 @@ import java.util.Optional;
 
 
 public interface ICuentaRepository extends JpaRepository<Cuenta, Long> {
-   // @Query("select c from Cuenta c where c.persona=?1")
-    //Optional<Cuenta> findByPersona(String persona);
+
+    @Query("select c from Cuenta c where c.persona=?1")
+    Optional<Cuenta> findByPersona(String persona);
 
 //    List<Cuenta> findAll();
 //    Cuenta findById(Long id);
