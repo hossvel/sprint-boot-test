faaacee4ad5db198ed3e643e52a9ebfc89081ce4
devhoss <velasco2288@gmail.com>
2023-09-08 12:11:05 -0500

    llamando a la excepcion

--- src/test/java/com/devhoss/app/SprintBootTestApplicationTests.java
+++ src/test/java/com/devhoss/app/SprintBootTestApplicationTests.java
@@ -1,5 +1,6 @@
 package com.devhoss.app;
 
+import com.devhoss.app.exceptions.DineroInsuficienteException;
 import com.devhoss.app.models.Banco;
 import com.devhoss.app.models.Cuenta;
 import com.devhoss.app.repositories.IBancoRepository;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -34,10 +36,13 @@ class SprintBootTestApplicationTests {
 		icuentaRepository = mock(ICuentaRepository.class);
 		ibancoRepository = mock(IBancoRepository.class);
 		icuentaService = new CuentaServiceImpl(icuentaRepository, ibancoRepository);
-
+		//para reiniciar la cuenta para cada metodo, debido a que falla cuando se modifica el mismo objeto
+		//Datos.CUENTA_001.setSaldo(new BigDecimal("1000"));
+        //Datos.CUENTA_002.setSaldo(new BigDecimal("2000"));
+        //Datos.BANCO.setTotalTransferencias(0);
 	}
 	@Test
-	void contextLoads() {
+	void contextLoads1() {
 		when(icuentaRepository.findById(1L)).thenReturn(Datos.crearCuenta001());
 		when(icuentaRepository.findById(2L)).thenReturn(Datos.crearCuenta002());
 		when(ibancoRepository.findById(1L)).thenReturn(Datos.crearBanco());
@@ -57,16 +62,60 @@ class SprintBootTestApplicationTests {
 		assertEquals("900", saldoOrigen.toPlainString());
 		assertEquals("2100", saldoDestino.toPlainString());
 
+		int total = icuentaService.revisarTotalTransferencias(1L);
+		assertEquals(1, total);
+
 		//verify de cuenta
 		verify(icuentaRepository, times(3)).findById(1L);
 		verify(icuentaRepository, times(3)).findById(2L);
 		verify(icuentaRepository, times(2)).update(any(Cuenta.class));
 
 		//verify de banco
-		verify(ibancoRepository, times(1)).findById(1L);
+		verify(ibancoRepository, times(2)).findById(1L);
 		verify(ibancoRepository).update(any(Banco.class));
 
 		verify(icuentaRepository, times(6)).findById(anyLong());
+		verify(icuentaRepository, never()).findAll();
+	}
+
+	@Test
+	void contextLoads2() {
+		when(icuentaRepository.findById(1L)).thenReturn(Datos.crearCuenta001());
+		when(icuentaRepository.findById(2L)).thenReturn(Datos.crearCuenta002());
+		when(ibancoRepository.findById(1L)).thenReturn(Datos.crearBanco());
+
+		//verificando los saldos de las cuentas inicialmente
+		BigDecimal saldoOrigen = icuentaService.revisarSaldo(1L);
+		BigDecimal saldoDestino = icuentaService.revisarSaldo(2L);
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		//transferir 1200  de una cuenta a otra pero no hay dinero
+
+		assertThrows(DineroInsuficienteException.class, ()-> {
+			icuentaService.transferir(1L, 2L, new BigDecimal("1200"), 1L);
+		});
+
+		//verificando los saldos despues de la transferencia
+		saldoOrigen = icuentaService.revisarSaldo(1L);
+		saldoDestino = icuentaService.revisarSaldo(2L);
+
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		int total = icuentaService.revisarTotalTransferencias(1L);
+		assertEquals(0, total);
+
+		//verify de cuenta
+		verify(icuentaRepository, times(3)).findById(1L);
+		verify(icuentaRepository, times(2)).findById(2L);
+		verify(icuentaRepository, never()).update(any(Cuenta.class));
+
+		//verify de banco
+		verify(ibancoRepository, times(1)).findById(1L);
+		verify(ibancoRepository,never()).update(any(Banco.class));
+
+		verify(icuentaRepository, times(5)).findById(anyLong());
 		verify(icuentaRepository, never()).findAll();
 	}
 
