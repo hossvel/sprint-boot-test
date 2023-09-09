eb07e5b39bf58236bbb8ce287fa19cc34ac80cdd
devhoss <velasco2288@gmail.com>
2023-09-08 19:36:18 -0500

    Configurando swager

--- src/main/java/com/devhoss/app/controllers/CuentaController.java
+++ src/main/java/com/devhoss/app/controllers/CuentaController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 import static org.springframework.http.HttpStatus.OK;
 
-@Controller
+@RestController
 @RequestMapping("/api/cuentas")
 public class CuentaController {
 
--- src/main/java/com/devhoss/app/services/CuentaServiceImpl.java
+++ src/main/java/com/devhoss/app/services/CuentaServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-@Service
+@Component
 public class CuentaServiceImpl implements ICuentaService{
 
     private ICuentaRepository icuentaRepository;
